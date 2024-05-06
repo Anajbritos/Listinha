4e24f948a549863ceae6db58001040e534dd4fb6
“Ana <anajbritos@gmail.com>
2024-05-06 12:24:41 -0300

    Criada camada de data com seu dto e entity do usuario

---
+++ src/main/java/io/ana/julia/listinha/data/userDTO/UserDTO.java
@@ -0,0 +1,53 @@
+package io.ana.julia.listinha.data.userDTO;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public UserDTO() {}
+    public UserDTO(
+            Long id,
+            String name,
+            String email,
+            String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+       this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+       return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+}
---
+++ src/main/java/io/ana/julia/listinha/data/userEntity/UserEntity.java
@@ -0,0 +1,61 @@
+package io.ana.julia.listinha.data.userEntity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public UserEntity() {}
+    public UserEntity(
+            Long id,
+            String name,
+            String email,
+            String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
