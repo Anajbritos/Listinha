734ae6bb6ee84be2ee6547b0a8cf5b9e9c9d765c
“Ana <anajbritos@gmail.com>
2024-05-08 12:44:46 -0300

    removendo @controller

--- src/main/java/io/ana/julia/listinha/controller/UserController.java
+++ src/main/java/io/ana/julia/listinha/controller/UserController.java
@@ -4,11 +4,10 @@ import io.ana.julia.listinha.data.dto.UserDto;
 import io.ana.julia.listinha.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@Controller
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
