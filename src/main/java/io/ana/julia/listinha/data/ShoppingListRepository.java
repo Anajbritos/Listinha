527e111cbfe0d893401cda609b5ee4d01270f994
“Ana <anajbritos@gmail.com>
2024-05-09 15:07:08 -0300

    Mudanca de uma regra na hora de salvar uma nova lista e atualizacao do repository

--- src/main/java/io/ana/julia/listinha/data/ShoppingListRepository.java
+++ src/main/java/io/ana/julia/listinha/data/ShoppingListRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Repository
 public interface ShoppingListRepository extends JpaRepository<ShoppingListEntity,Long> {
 
+     Boolean existsByDescriptionIgnoreCaseAndId(String description, Long id);
      Boolean existsByDescription(String description);
 
     List<ShoppingListEntity> findAllByUserId(Long userId);
